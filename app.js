03e3475e91c13f33b149974fa168593cd71213bc
muirurikin <muirurikin@gmail.com>
2017-01-29 13:08:28 +0300

    Added logo and Initialized views

--- app.js
+++ app.js
@@ -8,3 +8,25 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+ 
+ app.get('/', function(req, res) {
+     res.render('home');
+ });
+
+ app.get('/about', function(req, res) {
+     res.render('about');
+ });
+
+app.use(function(req, res) {
+    res.type('text/html');
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res. render('500');
+});
