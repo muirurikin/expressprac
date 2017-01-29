da462cfc043a3b24d289f77d333220a908d73e65
muirurikin <muirurikin@gmail.com>
2017-01-29 13:04:20 +0300

    Made imports and set up handlebars

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+
+app.disable('x-powered-by');
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
