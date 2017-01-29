ff3ad93c5cdb585be99de9021d74341e3175b624
muirurikin <muirurikin@gmail.com>
2017-01-29 13:24:13 +0300

    Added partial files and file-upload + functionality

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.use(require('body-parser').urlencoded({
+    extended: true }));
+
+var formidable = require('formidable');
+
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
@@ -27,6 +32,30 @@ app.use(express.static(__dirname + '/public'));
  app.get('/thankyou', function(req, res) {
      res.render('thankyou');
  });
+app.post('/process', function(req, res) {
+     console.log('Form : ' + req.query.form);
+     console.log('CSRF token : ' + req.body._csrf);
+     console.log('Email : ' + req.body.email);
+     console.log('Question : ' + req.body.ques);
+     res.redirect(303, '/thankyou');
+ });
+app.get('/file-upload', function(req, res) {
+     var now = new Date();
+     res.render('file-upload',{
+         year: now.getFullYear(),
+         month: now.getMonth() });
+ });
+
+app.post('/file-upload/:year/:month', function(req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, file) {
+        if(err)
+        return res.redirect(303, '/error');
+        console.log('File Received');
+        console.log(file);
+        res.redirect(303, '/thankyou');
+    });
+});
 
 app.use(function(req, res) {
     res.type('text/html');
